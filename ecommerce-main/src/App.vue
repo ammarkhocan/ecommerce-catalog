30a29a93cd29d7860b16b1e236c966c0d98d6600
ammarkhocan <ammarkhocan02@gmail.com>
2023-11-21 07:20:42 +0700

    first commit

---
+++ ecommerce-main/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <EcommerceProduct />
+</template>
+
+<script>
+import EcommerceProduct from "./components/EcommerceProduct.vue";
+
+export default {
+  name: "App",
+  components: {
+    EcommerceProduct,
+  },
+};
+</script>
+
+<style></style>
